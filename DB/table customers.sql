8e2a64cae1cacfb1746c3badbfb48021129eccd7
AsmaaShAli <asm_sh_a@hotmail.com>
2020-02-08 23:50:42 +0200

    put the files to repo

---
+++ DB/table customers.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM testdb.tbl_customer
+LIMIT 0, 1000
+
+-- Date: 2020-02-08 23:44
+*/
+INSERT INTO `tbl_customer` (`CustomerID`,`CustomerName`,`Address`,`City`,`Country`,`PostalCode`) VALUES (1,'Asmaa Sh. Ali','Fayoum ','Elfayoum','Egypt','12345');
+INSERT INTO `tbl_customer` (`CustomerID`,`CustomerName`,`Address`,`City`,`Country`,`PostalCode`) VALUES (2,'meme','apartment 17','Cairo','Egypt','54321');
---
+++ DB/table products.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM testdb.tbl_product
+LIMIT 0, 1000
+
+-- Date: 2020-02-08 23:45
+*/
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (1,'Samsung J2 Pro','1.jpg',100.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (2,'HP Notebook','2.jpg',299.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (3,'Panasonic T44 Lite','3.jpg',125.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (4,'Watch','4.jpg',200.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (5,'Bag','5.jpg',289.50);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (6,'Goggles','6.jpg',57.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (7,'Flip switch','7.jpg',190.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (8,'Flash light','8.jpg',500.00);
+INSERT INTO `tbl_product` (`id`,`name`,`image`,`price`) VALUES (9,'Headphones','9.jpg',80.00);
